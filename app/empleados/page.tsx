8add2b892ad263c6bf50d4f107a4afc8a282b59f
darihub <gramarg85@gmail.com>
2026-06-11 22:07:18 -0300

    comentar page.tsx

--- app/empleados/page.tsx
+++ app/empleados/page.tsx
@@ -10,31 +10,34 @@ import TablaEmpleados from "@/components/TablaEmpleados";
 import FiltrosEmpleados from "@/components/FiltrosEmpleados";
 
 export default function PaginaEmpleados() {
-  const [empleados, setEmpleados] = useState<Empleado[]>([]);
-  const [cargando, setCargando] = useState(true);
-  const [busqueda, setBusqueda] = useState("");
-  const [departamento, setDepartamento] = useState("");
+  const [empleados, setEmpleados] = useState<Empleado[]>([]); // guarda un array
+  const [cargando, setCargando] = useState(true); // guarda un booleano
+  const [busqueda, setBusqueda] = useState(""); // guarda un string.
+  const [departamento, setDepartamento] = useState(""); // guarda un string.
 
   const esAdmin = true; // hardcodeo temporal, hasta implementar roles
 
-  async function cargarEmpleados() {
-    setCargando(true);
+  async function cargarEmpleados() { // Función async permite que React siga funcionando mientras espera la respuesta.
+    setCargando(true); // hace que cargando = true. Esconde la tabla de empleados y muestra el html de "Cargando empleados..."
     try {
       const datos = await getEmpleados();
-      setEmpleados(datos);
+      setEmpleados(datos); // hace que empleados = [Juan, Ana, Pedro, ...]
     } catch (error) {
       console.error("Error cargando empleados", error);
     } finally {
-      setCargando(false);
+      setCargando(false); // Vuelve a mostrar la tabla de empleados.
     }
   }
 
   // Se ejecuta una sola vez cuando la página carga
   useEffect(() => {cargarEmpleados();}, []);
+  // useEffect() solo se ejecuta al cambiar el array de dependencias. Al estar vacio (es decir, []), está indicando que 
+  // este useEffect() no depende de nada, y debe ejecutarse una vez sola.
 
   // Filtrado en el cliente: no hace otra llamada a Firebase, filtra el array que ya está en memoria.
   const empleadosFiltrados = empleados.filter((emp) => {
     const coincideBusqueda = emp.nombre.toLowerCase().includes(busqueda.toLowerCase()) || emp.cargo.toLowerCase().includes(busqueda.toLowerCase());
+    // includes significa que incluye al menos una parte de lo que se busca. Es decir, si buscas "Juan", busqueda tiene parte de "Juan Perez".
     const coincideDepto = departamento === "" || emp.departamento === departamento;
 
     return coincideBusqueda && coincideDepto;
