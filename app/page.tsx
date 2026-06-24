4d428b784665ac0877fce496a5880110a9598f6e
darihub <gramarg85@gmail.com>
2026-06-24 13:46:54 -0300

    fix: borrar comentario antes de "use client" en app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,3 @@
-// app/page.tsx
-{/*import { db } from "@/lib/firebase";
-import { collection, getDocs } from "firebase/firestore";
-
-export default async function Home() {
-  try {
-    // Intenta leer la colección "empleados" (va a estar vacía, pero no debe dar error)
-    const snapshot = await getDocs(collection(db, "empleados"));
-    console.log("Firebase conectado. Documentos:", snapshot.size);
-  } catch (error) {
-    console.error("Error conectando Firebase:", error);
-  }
-
-  return (
-    <main>
-      <h1>Sistema de Empleados</h1>
-      <p>Firebase configurado. Ver consola del navegador.</p>
-    </main>
-  );
-}
-Codigo que usamos para testear se puede borrar.
-*/}
-
-
-// 
 "use client";
 
 import { useEffect, useState } from "react";
