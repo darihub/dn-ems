6d40559f4c1d66183af92bf0821aed801d0625a2
darihub <gramarg85@gmail.com>
2026-06-27 17:00:06 -0300

    fix: arreglar error params de next.js ocurrido en app/empleados/[id]/page.tsx

--- app/empleados/[id]/page.tsx
+++ app/empleados/[id]/page.tsx
@@ -7,24 +7,26 @@ import { Empleado } from "@/types/empleado";
 import FormularioEmpleado from "@/components/FormularioEmpleado";
 import RutaProtegida from "@/components/RutaProtegida";
 import Spinner from "@/components/Spinner";
+import { use } from "react";
 
 
 export default function PaginaEditarEmpleado({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
+  const { id } = use(params);
   const [empleado, setEmpleado] = useState<Empleado | null>(null);
 
   useEffect(() => {
     async function cargar() {
-      const snap = await getDoc(doc(db, "empleados", params.id));
+      const snap = await getDoc(doc(db, "empleados", id));
       if (snap.exists()) {
         setEmpleado({ id: snap.id, ...snap.data() } as Empleado);
       }
     }
     cargar();
-  }, [params.id]);
+  }, [id]);
 
   if (!empleado) return (
   <RutaProtegida soloAdmin={true}>
