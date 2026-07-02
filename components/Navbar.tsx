9483b5ef1a16cfdfb95d78305cd24afec698ed51
darihub <gramarg85@gmail.com>
2026-07-02 15:59:49 -0300

    change: efectuar rebrand del sistema a DNFlow

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -23,7 +23,7 @@ export default function Navbar(){
             <div className="max-w-7x1 mx-auto flex justify-between items-center">
                 {/* Logo / Nombre del sistema */}
                 <Link href="/" className="text-lg font-bold text-blue-600">
-                    HR System
+                    DNFlow
                 </Link>
 
                 {usuario ? (
